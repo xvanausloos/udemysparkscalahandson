42763c565ca399d137969f75ae8f5fc15ea7f404
Xavier VAN AUSLOOS <xavier.vanausloos@ladonneeintelligente.com>
2023-10-31 13:56:28 +0100

    module 17

--- src/main/scala/Module17MinTemp.scala
+++ src/main/scala/Module17MinTemp.scala
@@ -1,7 +1,19 @@
 package com.ldi.spark
 
 object Module17MinTemp {
+
+  def parseLine(line: String) = {
+    val fields = line.split(",")
+    val stationId = fields(0)
+    val entryType = fields(1)
+    val temperature = fields(2).toFloat * 0.1f * (9.0f / 5.0f) + 32.0f
+    (stationId, entryType, temperature)
+  }
+
   def main(args: Array[String]): Unit = {
+
+
+
 
   }
 
